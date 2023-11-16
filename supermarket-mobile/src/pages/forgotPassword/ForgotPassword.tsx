4c87b9e8489b601b6a5ed069ab9942cf421cab8d
Matheus Gomes <matt-xp2@live.com>
2023-11-15 21:58:20 -0300

    Corrigindo bugs

---
+++ supermarket-mobile/src/pages/forgotPassword/ForgotPassword.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const ForgotPassword: React.FC = () => {
+  // Seu código do componente ForgotPassword aqui
+  return (
+    <View>
+      <Text>Esqueci minha senha</Text>
+    </View>
+  );
+};
+
+export default ForgotPassword;
